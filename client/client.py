a207b31227032ceb5f5e11735af96e4fbf48292e
Berkem Billuroglu <berkem.billuroglu@holisticai.com>
2023-08-22 11:56:35 +0100

    asdfafda

--- client/client.py
+++ client/client.py
@@ -20,4 +20,4 @@ if __name__ == "__main__":
     # print(riskmapping_creation(project_result,risk_mapping_result,False))
     
     test_model = InferenceLoader("ParagraphFinder")
-    print(test_model.qa_predict('What is the colour of the sky?'))
+    print(test_model.paragraph_creator('I want to go chucky cheese'))
