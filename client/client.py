e3a2146cb9e34641db150c6cc6b970ff0cc85288
Berkem Billuroglu <berkem.billuroglu@holisticai.com>
2023-08-23 10:26:30 +0100

    test

--- client/client.py
+++ client/client.py
@@ -4,19 +4,12 @@ from inference import InferenceLoader
 if __name__ == "__main__":
     
 
-    # THRESHOLD_CS = 85 #0-100
-    # THRESHOLD_POST = 5 #0-10
-    # qa = QuestionAnsweringSystem(THRESHOLD_CS=THRESHOLD_CS,THRESHOLD_POST=THRESHOLD_POST)
-    
-    # file_list = ["Sova Overview Unilever.pdf","Sova Pilot.pdf"]
-    # question_json = 'questions.json'
-    # result = qa.run_pipeline(file_list ,question_json)
-    # print(f"len of {len(result)}")
-    # print(result)
-    # project_result,risk_mapping_result = answer_encapsulation(result)
-    
-    # print(riskmapping_creation(project_result,risk_mapping_result,False))
-    
-    test_model = InferenceLoader("ExtractiveQA")
+    # # successful Extractive QA
+    # test_model = InferenceLoader("ExtractiveQA")
+    # print(test_model.predict_SM(context='the weather is great today', question='what will the weather be like today?'))
 
-    print(test_model.predict_SM('the weather is great today', 'what will the weather be like today?'))
+    test_model = InferenceLoader("ParagraphFinder")
+    print(test_model.paragraph_creator('If my grandma had wheels she would have been a bike.\nBut shes doesnt have wheels'))
+    
+
+    
