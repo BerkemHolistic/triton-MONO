df8fc0cb1d9c594a21b743c37449d879d59f5829
Berkem Billuroglu <berkem.billuroglu@holisticai.com>
2023-08-24 11:22:22 +0100

    a

--- client/client.py
+++ client/client.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     # print(test_model.predict_SM(context='the weather is great today', question='what will the weather be like today?'))
 
     test_model = InferenceLoader("ParagraphFinder")
-    print(test_model.paragraph_creator('If my grandma had wheels she would have been a bike.\nBut shes doesnt have wheels'))
+    print(test_model.paragraph_creator(['If my grandma had wheels she would have been a bike.\nBut shes doesnt have wheels']))
     
 
     
